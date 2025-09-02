0ccba71c57d3d79b978225d3a4a4475dd19932c8
N.Upendhar <80301044+Upendhar10@users.noreply.github.com>
2025-09-02 21:51:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,3 +7,47 @@ const images = [
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
+
+// optional: trigger on button click
+btn.addEventListener("click", Main);
+
+function Main(){
+
+	// Clear old content
+	  output.innerHTML = "";
+	
+	// Loading Spinner
+	let loader = document.createElement('div');
+	loader.id = "loading";
+	loader.innerText = "Loading..."
+	output.append(loader);
+
+	Promise.all([
+		DownloadImage(images[0].url),
+		DownloadImage(images[1].url),
+		DownloadImage(images[2].url),
+	])
+	.then((img) => {
+		output.innerHTML = ""; // remove loader
+		output.append(...img);
+	})
+	.catch((err) => {
+		output.innerHTML = ""; // remove loader
+		
+		let error = document.createElement('div');
+		error.id = "error";
+		error.innerText = `${err.message}`
+		output.append(error);
+	})
+}
+
+function DownloadImage(imageURl){
+	return new Promise((resolve, reject) => {
+		const img = new Image();
+
+		img.onload = () => resolve(img); // image loaded successfully
+		img.onerror = () => reject(new Error(`Failed to load Image : ${imageURl}`))
+
+		img.src = imageURl;
+	})
+}
